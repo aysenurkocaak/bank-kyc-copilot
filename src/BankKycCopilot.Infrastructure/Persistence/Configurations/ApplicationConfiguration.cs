f4763945743452d3187108b28e7025ab1a2035d7
Aysenur Kocak <aysenur.kocak@ardgrup.com.tr>
2026-04-21 12:13:30 +0300

    align ef core relational version in api

---
+++ src/BankKycCopilot.Infrastructure/Persistence/Configurations/ApplicationConfiguration.cs
@@ -0,0 +1,50 @@
+using BankKycCopilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BankKycCopilot.Infrastructure.Persistence.Configurations;
+
+public class ApplicantConfiguration : IEntityTypeConfiguration<Applicant>
+{
+    public void Configure(EntityTypeBuilder<Applicant> builder)
+    {
+        builder.ToTable("applicants");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FullName)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.NationalId)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.Phone)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(x => x.BirthDate)
+            .IsRequired();
+
+        builder.Property(x => x.Country)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.City)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.Property(x => x.UpdatedAt);
+
+        builder.Property(x => x.IsDeleted)
+            .IsRequired();
+    }
+}
