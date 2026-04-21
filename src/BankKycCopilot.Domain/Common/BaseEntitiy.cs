8cd99e0aed0e54007d6766a41ea99800fcf48d5b
Aysenur Kocak <aysenur.kocak@ardgrup.com.tr>
2026-04-21 11:30:58 +0300

    add base entity

---
+++ src/BankKycCopilot.Domain/Common/BaseEntitiy.cs
@@ -0,0 +1,24 @@
+namespace BankKycCopilot.Domain.Common;
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+    public bool IsDeleted { get; protected set; }
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+    public void MarkAsUpdated()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void SoftDelete()
+    {
+        IsDeleted = true;
+        MarkAsUpdated();
+    }
+}
